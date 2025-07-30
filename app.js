2b2a6493f421fd187e182218202269d9f4f2beb2
Aditya Kumar <kumarsinghaditya240@gmail.com>
2025-07-30 22:39:05 +0530

    created index, new and create, show, edit,update, delete routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -19,6 +20,7 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 
 app.get("/", (req, res) => {
@@ -50,10 +52,32 @@ app.post("/listings", async (req, res) => {
     // let {title, description, image, price, country, location} = req.body;
     // let listing = req.body.listing;
     const newListing = new Listing(req.body.listing);
-    await newListing
-    console.log(listing);
+    await newListing.save();
+    res.redirect("/listings");
+    
+});
+
+// edit route
+app.get("/listings/:id/edit", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", {listing});
 })
 
+// update route
+app.put("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing}); //req.body.listing is the object sent from the form (e.g., { title: "Beach House", price: 200 }).{ //...req.body.listing } spreads all key-value pairs into a new object.
+    res.redirect("/listings");
+})
+
+// delete route
+app.delete("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+})
 
 app.listen(8080, () => {
     console.log("server is listening on port 8080");
